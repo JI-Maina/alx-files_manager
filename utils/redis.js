71e08648f503b93873e3bd1d26f6b4ef93f741e4
JI-Maina <irungujmaina@gmail.com>
2023-09-02 17:52:03 +0300

    Initial commit

---
+++ utils/redis.js
@@ -0,0 +1,38 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+
+class RedisClient {
+  constructor () {
+    this.connected = false;
+    this.client = createClient();
+
+    this.client.on('error', (err) => {
+      console.log(err);
+    });
+
+    this.client.on('connected', () => {
+      this.isClientConnected = true;
+    });
+  }
+
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return promisify(this.client.GET).bind(this.client)(key);
+  }
+
+  async set(key, value, duration) {
+    await promisify(this.client.SET).bind(this.client)(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    await promisify(this.client.DEL).bind(this.client)(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
