13f9d3c102bedd0961f2ea1c3c7077f05c234e9c
JI-Maina <irungu@gmail.com>
2023-09-05 09:15:04 -0700

    removed misplaced controller folder

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,11 +1,11 @@
-
+const dbClient = require('../utils/db');
 
 const postNew = (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  if (!email) res.status(400).json('Missing email');
-  if (!password) res.status(400).json('Missing password');
+  if (!email) res.status(400).json({ 'error': 'Missing email' });
+  if (!password) res.status(400).json({'error': 'Missing password' });
 
   // if email in db res.status(400).send('Already exist');
 
