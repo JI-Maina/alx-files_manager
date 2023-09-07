f68568ecaee4755267b9f21c84ec576a51d7fb89
BrianKvin <briankvin@gmail.com>
2023-09-07 06:13:35 -0700

    First File

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,7 +1,48 @@
 const dbClient = require('../utils/db')
 
-const postUpload = (req, res) => {
-  res.status(200).json(req.headers)
-}
+/**
+ * create a new file in db and disk
+ */
+
+// const postUpload = (req, res) => {
+  // res.status(200).json(req.headers)
+// }
+
+const async postUpload = (req, res) => {
+  const { userId } = await userUtils.getIdAndKey(req);
+  
+  if (!basicUtils.isValid(userId)) {
+    return response.status(401).send({ error: 'Unauthorized' });
+  }
+
+  if (!userId && req.body.type == 'image') {
+    await fileQueue.add({});
+  }
+
+  const user = await userUtils.getUser({
+    _id: ObjectId(userId),
+  });
+
+  if (!user) return response.status(401).send({ error: 'Unauthorized' });
+
+  const { error: validationError, fileParams } = await fileUtils.validateBody(req,);
+
+  if (fileParams.parentId !== 0 && !basicUtils.isValidId(fileParams.parentId)) { return response.status(400).send({ error: 'Parent not found' }); }
+
+  const { error, code, newFile } = await fileUtils.saveFile(
+    userId,
+    fileParams,
+    FOLDER_PATH,
+  );
+
+  if (fileParams.type == 'image') {
+    await fileQueue.add({
+      fileId: newFile.id.toString(),
+      userId: newFile.userId.toString(),
+    });
+  }
+
+  return response.status(201).send(newFile);
+};
 
 module.exports = { postUpload }
