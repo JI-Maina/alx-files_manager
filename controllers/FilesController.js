77355e65bcd338e5a76c136e51bd5c39d107de27
BrianKvin <briankvin@gmail.com>
2023-09-08 03:11:19 -0700

    Get and list file

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -42,7 +42,77 @@ const postUpload = async (req, res) => {
   return res.status(401).json({ error: 'Unauthorized' });
 }
 
+/**
+ * should retrieve the file document based on the ID
+ */
+
+const getShow = async (req, res) => {
+  const token = req.headers['x-token'];
+  const key = `auth_${token}`;
+  const userObj = await redisClient.get(key);
+
+  if (!userObj) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const fileId = req.params.id;
+
+  const collection = await dbClient.filesCollection();
+  const file = await collection.findOne({
+    _id: ObjectId(fileId),
+    userId: userObj.id,
+  });
+
+  if (!file) {
+    return res.status(404).json({ error: 'Not found' });
+  }
+
+  return res.status(200).json(file);
+};
+
+
+/**
+ * retrieve a list of file documents with pagination
+ */
+
+const getIndex = async (req, res) => {
+  const token = req.headers['x-token'];
+  const key = `auth_${token}`;
+  const userObj = await redisClient.get(key);
+
+  if (!userObj) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const parentId = req.query.parentId || 0;
+  const page = parseInt(req.query.page) || 0;
+  const perPage = 20; // Items per page
+
+  const collection = await dbClient.filesCollection();
+
+  const pipeline = [
+    {
+      $match: {
+        userId: userObj.id,
+        parentId: ObjectId(parentId),
+      },
+    },
+    {
+      $skip: page * perPage,
+    },
+    {
+      $limit: perPage,
+    },
+  ];
+
+  const files = await collection.aggregate(pipeline).toArray();
+
+  return res.status(200).json(files);
+};
+
 module.exports = {
   postUpload,
+  getShow,
+  getIndex,
 }
 
